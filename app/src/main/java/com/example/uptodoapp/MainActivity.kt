c0531a37bb90c9ed31ba7f3afb21d9a95d6c30c8
chethanv5 <velechatichethan@gmail.com>
2024-08-12 12:07:57 +0530

    Finished project

--- app/src/main/java/com/example/uptodoapp/MainActivity.kt
+++ app/src/main/java/com/example/uptodoapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.uptodoapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
